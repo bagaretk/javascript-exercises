a9992eecbf5bb4372096fa075ccd68f5e2314093
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-03-14 01:08:49 +0200

    ex.02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,numberOfTimes) {
+    if(numberOfTimes < 0) {
+        return "ERROR";
+    }
+    let finalString='';
+    for(let i=0;i < numberOfTimes; i++){
+        finalString += word;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
