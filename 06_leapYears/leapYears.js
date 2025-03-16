d5a1e04a5a20d4b87308afc2087b529818c302b8
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-03-16 19:51:47 +0200

    ex.06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isDivisibleByFour, isDivisibleByHundred, isDivisibleByFourHundred;
+    if(((year % 4 == 0) && (year % 100 != 0)) || ((year % 100 == 0 ) && (year % 400 == 0)))
+        return true;
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
