8c441ab5240ab51976dc27b81004d7ff6b32bcb9
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-03-16 21:00:27 +0200

    ex.09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyzx0123456789";
+    const onlyCharacters = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+    let reversedString = onlyCharacters.split('').reverse().join('');
 
+    return onlyCharacters === reversedString;
 };
 
 // Do not edit below this line
