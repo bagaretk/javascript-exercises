2029555ca18b367d35b8fe9f638ce03fb70ff6ac
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-03-16 02:20:48 +0200

    ex.04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementToDeletes) {
+    elementToDeletes.forEach(elementToDelete => {
+        let index;
+        while((index = array.indexOf(elementToDelete)) != -1){
+            console.log(index);
+            array.splice(index,1);
+    }});
+    
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
