9f868a71fed91835f1f039ec32b0e2054c462f92
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-03-16 19:45:49 +0200

    ex.05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    if(start < 0 || finish < 0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(start) || !Number.isInteger(finish)){
+        return "ERROR";
+    }
 
+    if (start > finish){
+        let aux = start;
+        start = finish;
+        finish = aux;
+    }
+    let sum=0;
+    for(let i=start; i<=finish; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
