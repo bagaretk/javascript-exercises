76fd5dd979e77a7ca458b11485b7e02a459e7f7a
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-03-16 20:20:08 +0200

    ex.08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(numOne, numTwo) {
+  return numOne - numTwo;
 
 };
 
-const power = function() {
-	
+const sum = function(arrayOfNumbers) {
+	let sum = 0;
+  arrayOfNumbers.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const factorial = function() {
-	
+const multiply = function(arrayOfNumbers) {
+  let prod = 1;
+  arrayOfNumbers.forEach(element => {
+    prod *= element;
+  });
+  return prod;
+};
+
+const power = function(numOne, numTwo) {
+	let number = 1;
+  if(numTwo == 0){
+    return 1;
+  }
+  while(numTwo > 0){
+    number *= numOne;
+    numTwo--;
+  }
+  return number;
+};
+
+const factorial = function(number) { 
+  let product = 1;
+  for (let i = number; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
